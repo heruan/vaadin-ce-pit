392a7de2c7dbfa59b66e11c91feb01408d10fb4a
Giovanni Lovato <giovanni@lova.to>
2021-08-11 10:47:14 +0200

    Add database configuration

---
+++ src/main/java/com/example/application/data/Person.java
@@ -0,0 +1,28 @@
+package com.example.application.data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
